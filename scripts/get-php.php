89012620c3edd368b0ee8d2249cb9076962f6fe8
PhysK <chris@physk.co.uk>
2017-10-01 21:07:26 +0100

    Removed echo statment and added comments

--- scripts/get-php.php
+++ scripts/get-php.php
@@ -1,11 +1,15 @@
 <?php
 
+//Enter Forum Post URL here
 $forumurl = "https://www.makemkv.com/forum2/viewtopic.php?f=5&t=1053";
 
+//Grabs the HTML Content from the server
 $html = file_get_contents($forumurl);
-//echo $html;
+
+//Finds the new app code
 preg_match("'<div class=\"codecontent\">(.*?)</div>'si", $html, $matches);
 
+//If app code exists write to tmpcode
 if(is_array($matches))
 {
 	$fp = fopen("tmpcode", "w");
