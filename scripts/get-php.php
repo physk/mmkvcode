cc7711bdd7a5f5cb58424ac6e1e5de6bb0bc5d75
PhysK <chris@physk.co.uk>
2017-10-01 21:02:39 +0100

    Added PHP Script Content

---
+++ scripts/get-php.php
@@ -0,0 +1,14 @@
+<?php
+
+$forumurl = "https://www.makemkv.com/forum2/viewtopic.php?f=5&t=1053";
+
+$html = file_get_contents($forumurl);
+//echo $html;
+preg_match("'<div class=\"codecontent\">(.*?)</div>'si", $html, $matches);
+print_r($matches);
+if(is_array($matches))
+{
+	$fp = fopen("tmpcode", "w");
+	fwrite($fp, $matches[1]);
+	fclose($fp);
+}
